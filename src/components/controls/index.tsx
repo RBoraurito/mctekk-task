b49f366aa36ccb21724f48d84295b962846576ce
RBoraure <ricardo.boraure@bejamas.io>
2024-06-21 18:29:59 -0400

    🎨 chore: format code

--- src/components/controls/index.tsx
+++ src/components/controls/index.tsx
@@ -3,24 +3,35 @@ interface ControlsProps {
   setSize: (value: number) => void;
 }
 
-export function Controls({size, setSize}: ControlsProps) {
+export function Controls({ size, setSize }: ControlsProps) {
   function handleIncrement() {
-    setSize(size + 1)
+    setSize(size + 1);
   }
 
   function handleDecrement() {
-    setSize(size - 1)
+    setSize(size - 1);
   }
 
   return (
     <div className="flex gap-2">
-      <button onClick={handleIncrement} className="rounded-lg border border-slate-50 px-3 py-1 hover:bg-slate-50 hover:text-slate-900 bg-slate-800">
+      <button
+        onClick={handleIncrement}
+        className="rounded-lg border border-slate-50 bg-slate-800 px-3 py-1 hover:bg-slate-50 hover:text-slate-900"
+      >
         +
       </button>
-      <input type="text" readOnly value={size} className="rounded text-slate-900 text-center w-10" />
-      <button onClick={handleDecrement} className="rounded-lg border border-slate-50 px-3 py-1 hover:bg-slate-50 hover:text-slate-900 bg-slate-800">
+      <input
+        type="text"
+        readOnly
+        value={size}
+        className="w-10 rounded text-center text-slate-900"
+      />
+      <button
+        onClick={handleDecrement}
+        className="rounded-lg border border-slate-50 bg-slate-800 px-3 py-1 hover:bg-slate-50 hover:text-slate-900"
+      >
         －
       </button>
     </div>
-  )
+  );
 }
--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -15,20 +15,23 @@ export function Editor() {
 
   async function download() {
     var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/html;charset=utf-8,' + encodeURIComponent(await marked.parse(markdown)));
+    element.setAttribute(
+      'href',
+      'data:text/html;charset=utf-8,' + encodeURIComponent(await marked.parse(markdown)),
+    );
     element.setAttribute('download', 'exported-html');
-  
+
     element.style.display = 'none';
     document.body.appendChild(element);
-  
+
     element.click();
-  
+
     document.body.removeChild(element);
   }
   return (
     <section className="">
       <div className="mx-auto grid h-[80vh] max-w-6xl px-4 lg:grid-cols-2 lg:px-8">
-        <div className="border border-slate-100 overflow-auto">
+        <div className="overflow-auto border border-slate-100">
           <CodeEditor
             value={markdown}
             language="md"
@@ -36,23 +39,26 @@ export function Editor() {
             onChange={(evn) => setMarkdown(evn.target.value)}
             padding={15}
             style={{
-              fontSize: "18px"
+              fontSize: '18px',
             }}
             className="m-0 h-full w-full p-3 text-lg"
           />
         </div>
-        <div className="flex flex-col border border-slate-100 text-slate-50 overflow-auto">
+        <div className="flex flex-col overflow-auto border border-slate-100 text-slate-50">
           <ThemeSelector theme={theme} setTheme={setTheme}>
             <Controls size={size} setSize={setSize} />
           </ThemeSelector>
           <div
-            style={{'--text-base': `${size}px`} as CSSProperties}
+            style={{ '--text-base': `${size}px` } as CSSProperties}
             className={clsx('p-3', styles.container, styles[theme])}
             dangerouslySetInnerHTML={{ __html: marked.parse(markdown) }}
           ></div>
         </div>
       </div>
-      <button onClick={download} className='px-4 py-2 border border-slate-400 bg-slate-100 rounded-xl text-slate-900 hover:bg-opacity-50 mx-auto my-12 block text-2xl font-bold'>
+      <button
+        onClick={download}
+        className="mx-auto my-12 block rounded-xl border border-slate-400 bg-slate-100 px-4 py-2 text-2xl font-bold text-slate-900 hover:bg-opacity-50"
+      >
         Download HTML
       </button>
     </section>
